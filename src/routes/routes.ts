f1ea7a38aae16cf00a6db5b0d125dd2af2f70d8e
Alan Gonzalez <74994720+AlanDev24@users.noreply.github.com>
2024-08-06 16:42:34 -0700

    fix code

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -10,9 +10,9 @@ interface Route {
 }
 
 //? lo que esta entre comentarios sirve para renombrar los chunks
-const lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage1*/"../lazyload/pages/LazyPages2"));
-const lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage2*/"../lazyload/pages/LazyPages3"));
-const lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage3*/"../lazyload/pages/LazyPages"));
+const lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1*/"../lazyload/pages/LazyPages"));
+const lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2*/"../lazyload/pages/LazyPages2"));
+const lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3*/"../lazyload/pages/LazyPages3"));
 
 export const routes: Route[] = [
   {
